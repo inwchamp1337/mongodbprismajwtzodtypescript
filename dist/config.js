a26d76fce474348a24feb2fc70dfe4935aa195ad
inwchamp1337 <115950933+inwchamp1337@users.noreply.github.com>
2025-05-07 14:59:10 +0700

    Delete dist directory

--- dist/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-require("dotenv/config");
-exports.config = {
-    JWT_SECRET: process.env.JWT_SECRET,
-    PORT: process.env.PORT || 3000,
-};
