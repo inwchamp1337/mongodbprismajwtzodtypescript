393c8d7784f689bce3b5632809e7704d004139ad
inwchamp1337 <ppasinb@gmail.com>
2025-05-08 22:09:17 +0700

    Update auth.controller.ts

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express'
 import * as authService from './auth.service'
+import { RegisterInput } from './auth.schemas'
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (
+    req: Request<{}, {}, RegisterInput>,
+    res: Response
+) => {
     try {
         const { user, token } = await authService.register(req.body)
         res.status(201).json({
@@ -9,8 +13,12 @@ export const register = async (req: Request, res: Response) => {
             data: { user, token }
         })
     } catch (error: any) {
+<<<<<<< HEAD
         // ปรับปรุงการจัดการ error
         if (error.message.includes('exists')) {
+=======
+        if (error.message?.includes('exists')) {
+>>>>>>> NEW-schema_update
             return res.status(409).json({
                 success: false,
                 message: error.message
